f0e56dc9b92abac82c72fad340009a6a4c1689d7
fril achraf <frilachraf.dev@gmail.com>
2025-09-05 15:40:38 +0100

    supabase setuped succesfully

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Timeline3 } from './AppTimeline3';
+import { createClient } from '@/lib/supabase/server';
 
 interface Project {
     title: string;
@@ -11,11 +12,17 @@ interface ProjectsSectionProps {
     projects: Project[];
 }
 
-const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => (
+const ProjectsSection: React.FC<ProjectsSectionProps> = async ({ projects }) => 
+  { 
+   const supabase = await createClient();
+  const { data: instruments } = await supabase.from("projects").select();
+ 
+    return  (
     <section>
         <h2>Projects</h2>
         <Timeline3 />
+        {<pre>{JSON.stringify(instruments, null, 2)}</pre>}
     </section>
-);
+)}
 
 export default ProjectsSection;
